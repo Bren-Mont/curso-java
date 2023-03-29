c52da813c5e102582eb9d2905ce60c14f6cae4af
Bren-Mont <bremonterosa@gmail.com>
2023-03-29 16:16:52 -0500

    operadores

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,26 +2,12 @@
 
     public class HolaMundo {
         public static void main(String args[]) {
-            System.out.println("Proporciona el nombre");
-            Scanner consola = new Scanner(System.in);
-            var nombre= consola.nextLine();
-
-            System.out.println("Proporciona el id");
-            var id= Integer.parseInt(consola.nextLine());
-            System.out.println(nombre + id);
-
-
-            System.out.println("Proporciona el precio");
-            var precio= Integer.parseInt(consola.nextLine());
-            System.out.println("$" + " "+ precio);
-
-            System.out.println("Proporciona el envio gratuito:");
-            boolean envioGratuito= precio >= 3000;
-            if (envioGratuito){
-                System.out.println("el envio es gratuito");
+            int a= 5, b=10;
+            if(b%2==0){
+                System.out.println("es par");
             }
-            else {
-                System.out.println("no es gratuito");
+            else{
+                System.out.println("no es par");
             }
         }
     }
